00d922231137f7663e024095b25e774f23bdf970
taisei-101010 <tainii.other@gmail.com>
2021-12-13 23:41:57 +0900

    Added login function.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,10 +2,13 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
-end
 
-private
+  belongs_to :user
 
-def validate_name_not_including_comma
-  errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+
 end
