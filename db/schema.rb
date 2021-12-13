756f2bedc5e5174b89d31a5182a18595c84c8be0
taisei-101010 <tainii.other@gmail.com>
2021-12-13 15:54:16 +0900

    Add validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_050028) do
+ActiveRecord::Schema.define(version: 2021_12_13_054240) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
